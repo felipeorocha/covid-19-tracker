7975acb32605d96d64828e6afea6716a81ed539f
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-04-07 19:36:36 -0300

    Cards: include cards component logic and structure(passing props data to cards)

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Grid } from '@material-ui/core';
+import Card from '../Card';
 
 import styles from './cards.module.css';
 
-const Cards = props =>(
-  <div className={styles.container}>
-    <Grid container spacing={3} justify="center">
-      <Grid item component={Card}>
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>Infectados</Typography>
-          <Typography variant="h5">REAL DATA</Typography>
-          <Typography color="textSecondary">REAL DATE</Typography>
-          <Typography variant="body2">Numero de infectados de COVID-19</Typography>
-        </CardContent>
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return <h3>Loading...</h3>;
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Card
+          numbers={confirmed.value}
+          style={styles.infected}
+          status="Infectados"
+          date={lastUpdate}
+        />
+        <Card
+          numbers={recovered.value}
+          style={styles.recovered}
+          status="Curados"
+          date={lastUpdate}
+        />
+        <Card
+          numbers={deaths.value}
+          style={styles.deaths}
+          status="Mortos"
+          date={lastUpdate}
+        />
       </Grid>
-    </Grid>
-  </div>
-);
+    </div>
+  );
+};
+
+Cards.propTypes = {
+  data: PropTypes.shape({
+    confirmed: PropTypes.object,
+    recovered: PropTypes.object,
+    deaths: PropTypes.object,
+    lastUpdate: PropTypes.string,
+  }),
+};
+
+Cards.defaultProps = {
+  data: {},
+};
 
 export default Cards;
