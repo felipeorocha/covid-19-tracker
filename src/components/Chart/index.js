ca277f9c2157b4795445c8236a91836484df3de7
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-04-07 19:33:28 -0300

    Charts: includes infected/death graphics chart and chart styles

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+import { fetchDailyData } from '../../api';
 
-const Chart = () => <h1>Chart</h1>;
+import styles from './chart.module.css';
+
+const Chart = () => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  }, []);
+
+  const lineChart =
+    dailyData.length !== 0 ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [
+            {
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: 'Infectados',
+              borderColor: '#3333ff',
+              fill: true,
+            },
+            {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: 'Mortos',
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              fill: true,
+            },
+          ],
+        }}
+      />
+    ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
+};
 
 export default Chart;
